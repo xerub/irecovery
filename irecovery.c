147e8c05c3673ae2d52fd8d14915db22c45cb1a3
Joshua Hill <posixninja@storm.(none)>
2009-10-14 02:04:44 -0400

    I need to learn to test before pushing

--- irecovery.c
+++ irecovery.c
@@ -304,7 +304,7 @@ void irecv_usage(void) {
 	printf("\t-f <file>\t\tupload file.\n");
 	printf("\t-r\t\t\treset usb.\n");
 	printf("\t-c \"command\"\t\tsends a single command.\n");
-	printf("\t-s\t\t\tstarts a shell.\n\n");
+	printf("\t-s\t\t\tstarts a shell.\n");
 	printf("\t-k\t\t\tsend usb exploit.\n\n");
 }
 
