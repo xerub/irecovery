9059ecb80a4dbe7d481dc25787c450bf7fb4312e
Joshua Hill <posixninja@storm.(none)>
2010-03-05 04:09:29 -0500

    merged ih8sn0w branch and applied a few of my own changes

--- irecovery.c
+++ irecovery.c
@@ -24,24 +24,24 @@
 #include <readline/history.h>
 #include <usb.h>
 
-#define VENDOR_ID    0x05AC
-#define NORM_MODE    0x1290
-#define RECV_MODE    0x1281
-#define WTF_MODE     0x1227
-#define DFU_MODE     0x1222
-#define BUF_SIZE     0x10000
+#define VENDOR_ID       0x05AC
+#define NORM_MODE       0x1290
+#define RECV_MODE       0x1281
+#define WTF_MODE        0x1227
+#define DFU_MODE        0x1222
+#define BUF_SIZE        0x10000
 
-void irecv_hexdump(char *buf, int len) {
-	int i = 0;
-	for (i = 0; i < len; i++) {
-		if (i % 16 == 0 && i != 0)
+void irecv_hexdump(unsigned char* buf, unsigned int len) {
+	unsigned int i = 0;
+	for(i = 0; i < len; i++) {
+		if(i % 16 == 0 && i != 0)
 			printf("\n");
 		printf("%02x ", buf[i]);
 	}
 	printf("\n");
 }
 
-struct usb_dev_handle *irecv_init(int devid) {
+struct usb_dev_handle* irecv_init(unsigned int devid) {
 	struct usb_dev_handle *handle = NULL;
 	struct usb_device *dev = NULL;
 	struct usb_bus *bus = NULL;
@@ -63,33 +63,38 @@ struct usb_dev_handle *irecv_init(int devid) {
 	return NULL;
 }
 
-void irecv_close(struct usb_dev_handle *handle) {
+void irecv_close(struct usb_dev_handle* handle) {
 	printf("Closing USB connection...\n");
 	if (handle != NULL) {
 		usb_close(handle);
 	}
 }
 
-void irecv_reset(struct usb_dev_handle *handle) {
+void irecv_reset(struct usb_dev_handle* handle) {
 	if (handle != NULL) {
 		usb_reset(handle);
 	}
 }
 
-int irecv_sendfile(struct usb_dev_handle *handle, char *filename) {
+int irecv_upload(struct usb_dev_handle* handle, char* filename) {
+	if(handle == NULL) {
+		printf("irecv_upload: Device has not been initialized!\n");
+		return -1;
+	}
+	
 	FILE* file = fopen(filename, "rb");
-	if (file == NULL) {
-		printf("File %s not found.\n", filename);
+	if(file == NULL) {
+		printf("irecv_upload: Unable to find file!\n");
 		return 1;
 	}
 
 	fseek(file, 0, SEEK_END);
-	int len = ftell(file);
+	long len = ftell(file);
 	fseek(file, 0, SEEK_SET);
 
 	char* buffer = malloc(len);
 	if (buffer == NULL) {
-		printf("Error allocating memory!\n");
+		printf("irecv_upload: Error allocating memory!\n");
 		fclose(file);
 		return 1;
 	}
@@ -98,44 +103,48 @@ int irecv_sendfile(struct usb_dev_handle *handle, char *filename) {
 	fclose(file);
 
 	int packets = len / 0x800;
-	if (len % 0x800) {
+	if(len % 0x800) {
 		packets++;
 	}
 
 	int last = len % 0x800;
-	if (!last) {
+	if(!last) {
 		last = 0x800;
 	}
 
 	int i = 0;
 	char response[6];
-	for (i = 0; i < packets; i++) {
+	for(i = 0; i < packets; i++) {
 		int size = i + 1 < packets ? 0x800 : last;
 
-		if (!usb_control_msg(handle, 0x21, 1, i, 0, &buffer[i * 0x800], size,
-				1000)) {
-			printf("Error!\n");
+		if(!usb_control_msg(handle, 0x21, 1, i, 0, &buffer[i * 0x800], size, 1000)) {
+			printf("irecv_upload: Error sending packet!\n");
+			return -1;
 		}
 
-		if (usb_control_msg(handle, 0xA1, 3, 0, 0, response, 6, 1000) != 6) {
-			printf("Error receiving status!\n");
+		if(usb_control_msg(handle, 0xA1, 3, 0, 0, response, 6, 1000) != 6) {
+			printf("irecv_upload: Error receiving status!\n");
+			return -1;
 
 		} else {
-			printf(".");
-			if (response[4] != 5) {
-				printf("Status error!\n");
+			if(response[4] != 5) {
+				printf("irecv_upload: Invalid status error!\n");
+				return -1;
 			}
 		}
+
 	}
 
-	printf("\n");
 	usb_control_msg(handle, 0x21, 1, i, 0, buffer, 0, 1000);
-	for (i = 6; i <= 8; i++) {
-		if (usb_control_msg(handle, 0xA1, 3, 0, 0, response, 6, 1000) != 6) {
-			printf("Error receiving status!\n");
+	for(i = 6; i <= 8; i++) {
+		if(usb_control_msg(handle, 0xA1, 3, 0, 0, response, 6, 1000) != 6) {
+			printf("irecv_upload: Error receiving execution status!\n");
+			return -1;
+
 		} else {
-			if (response[4] != i) {
-				printf("Status error!\n");
+			if(response[4] != i) {
+				printf("irecv_upload: Invalid execution status!\n");
+				return -1;
 			}
 		}
 	}
@@ -144,55 +153,55 @@ int irecv_sendfile(struct usb_dev_handle *handle, char *filename) {
 	return 0;
 }
 
-int irecv_sendbuffer(struct usb_dev_handle* handle, char* data, int len) {
-	if (!handle) {
-		printf("Device has not been initialized!\n");
-		return 1;
+int irecv_buffer(struct usb_dev_handle* handle, char* data, int len) {
+	if(handle == NULL) {
+		printf("irecv_buffer: Device has not been initialized!\n");
+		return -1;
 	}
 
 	int packets = len / 0x800;
-	if (len % 0x800) {
+	if(len % 0x800) {
 		packets++;
 	}
 
 	int last = len % 0x800;
-	if (!last) {
+	if(!last) {
 		last = 0x800;
 	}
 
 	int i = 0;
 	char response[6];
-	for (i = 0; i < packets; i++) {
+	for(i = 0; i < packets; i++) {
 		int size = i + 1 < packets ? 0x800 : last;
 
-		if (!usb_control_msg(handle, 0x21, 1, i, 0, &data[i * 0x800], size,
-				1000)) {
-			printf("error!\n");
+		if(!usb_control_msg(handle, 0x21, 1, i, 0, &data[i * 0x800], size, 1000)) {
+			printf("irecv_buffer: Error sending packet!\n");
+			return -1;
 		}
 
-		if (usb_control_msg(handle, 0xA1, 3, 0, 0, response, 6, 1000) != 6) {
-			printf("error receiving send status!\n");
-			return 1;
+		if(usb_control_msg(handle, 0xA1, 3, 0, 0, response, 6, 1000) != 6) {
+			printf("irecv_buffer: Error receiving status!\n");
+			return -1;
 
 		} else {
-			if (response[4] != 5) {
-				printf("send status error!\n");
-				return 1;
+			if(response[4] != 5) {
+				printf("irecv_buffer: Invalid status error!\n");
+				return -1;
 			}
 		}
 
 	}
 
 	usb_control_msg(handle, 0x21, 1, i, 0, data, 0, 1000);
-	for (i = 6; i <= 8; i++) {
-		if (usb_control_msg(handle, 0xA1, 3, 0, 0, response, 6, 1000) != 6) {
-			printf("error receiving execution status!\n");
-			return 1;
+	for(i = 6; i <= 8; i++) {
+		if(usb_control_msg(handle, 0xA1, 3, 0, 0, response, 6, 1000) != 6) {
+			printf("irecv_buffer: Error receiving execution status!\n");
+			return -1;
 
 		} else {
-			if (response[4] != i) {
-				printf("execution status error!\n");
-				return 1;
+			if(response[4] != i) {
+				printf("irecv_buffer: Invalid execution status!\n");
+				return -1;
 			}
 		}
 	}
@@ -200,172 +209,206 @@ int irecv_sendbuffer(struct usb_dev_handle* handle, char* data, int len) {
 	return 0;
 }
 
-int irecv_sendcmd(struct usb_dev_handle *handle, char *command) {
+int irecv_command(struct usb_dev_handle *handle, int argc, char* argv[]) {
+    char* command = argv[0];
 	size_t length = strlen(command);
-	if (length >= 0x200) {
-		printf("Error command is too long!\n");
-		return 1;
+	if(length >= 0x200) {
+		printf("irecv_command: Error command is too long!\n");
+		return -1;
 	}
 
-	if (!usb_control_msg(handle, 0x40, 0, 0, 0, command, length + 1, 1000)) {
-		printf("[!] %s", usb_strerror());
+	if(!usb_control_msg(handle, 0x40, 0, 0, 0, command, length + 1, 1000)) {
+		printf("irecv_command: Error sending command!\n");
+		return -1;
 	}
 
 	return 0;
 }
 
-int irecv_parsecmd(struct usb_dev_handle *handle, char *command) {
-	char* action = strtok(strdup(command), " ");
-	if (strcmp("/sendfile", action) == 0) {
-		char* filename = strtok(NULL, " ");
-		if (filename != NULL) {
-			irecv_sendfile(handle, filename);
-		}
+int irecv_exploit(struct usb_dev_handle* handle, char* payload) {
+    if(handle == NULL) {
+		printf("irecv_exploit: Device has not been initialized!\n");
+		return -1;
 	}
-
-	if (strcmp("/usbhax", action) == 0) {
-			printf("usbhax 0x21-2-0-0: %04x\n", usb_control_msg(handle, 0x21, 2, 0,
-																0, 0, 0, 1000));   
-		}
+	
+    if(payload != NULL) {
+        if(irecv_upload(handle, payload) < 0) {
+            printf("irecv_exploit: Error uploading payload!\n");
+            return -1;
+        }
+    }
+    
+    if(!usb_control_msg(handle, 0x21, 2, 0, 0, 0, 0, 1000)) {
+		printf("irecv_exploit: Error sending exploit!\n");
+		return -1;
+	}
+	
+    return 0;
 }
 
-void irecv_console(struct usb_dev_handle *handle) {
-	if (usb_set_configuration(handle, 1) < 0) {
-		printf("%s\n", usb_strerror());
-		return;
+int irecv_parse(struct usb_dev_handle* handle, char* command) {
+    unsigned int status = 0;
+	char* action = strtok(strdup(command), " ");
+	if(!strcmp(action, "exit")) {
+	    free(action);
+	    return -1;
+	    
+	} else if(!strcmp(action, "help")) {
+	    printf("Commands:\n");
+	    printf("\t/upload <file>\t\tupload file in shell\n");
+	    printf("\t/exploit [payload]\t\tsend usb exploit in shell.\n");
+	
+	} else if(strcmp(action, "upload") == 0) {
+		char* filename = strtok(NULL, " ");
+		if(filename != NULL) {
+			irecv_upload(handle, filename);
+		}
+		
+	} else if(strcmp(action, "exploit") == 0) {
+		char* payload = strtok(NULL, " ");
+		if (payload != NULL) {
+			irecv_exploit(handle, payload);
+		} else {
+		    irecv_exploit(handle, NULL);
+		}
+	}
+	
+	free(action);
+	return 0;
+}
+
+int irecv_console(struct usb_dev_handle *handle, char* logfile) {
+	if(usb_set_configuration(handle, 1) < 0) {
+	    printf("irecv_console: Error setting configuration!\n");
+		return -1;
 	}
 
-	if (usb_claim_interface(handle, 1) < 0) {
-		printf("%s\n", usb_strerror());
-		return;
+	if(usb_claim_interface(handle, 1) < 0) {
+	    printf("irecv_console: Error claiming interface!\n");
+		return -1;
 	}
 
-	if (usb_set_altinterface(handle, 1) < 0) {
-		printf("%s\n", usb_strerror());
-		return;
+	if(usb_set_altinterface(handle, 1) < 0) {
+	    printf("irecv_console: Error setting alt interface!\n");
+		return -1;
 	}
 
 	char* buffer = malloc(BUF_SIZE);
-	if (buffer == NULL) {
-		printf("Error allocating memory!\n");
-		return;
+	if(buffer == NULL) {
+		printf("irecv_console: Error allocating memory!\n");
+		return -1;
 	}
 	
-	FILE* fd = fopen("irecovery.log", "w");
-	if(fd == NULL) {
-	    printf("Unable to open log file!\n");
-	    return;
+	FILE* fd = NULL;
+	if(logfile != NULL) {
+	    fd = fopen("irecovery.log", "w");
+	    if(fd == NULL) {
+	        printf("irecv_console: Unable to open log file!\n");
+	        free(buffer);
+	        return -1;
+	    }
 	}
 
-	while (1) {
+	while(1) {
 		int bytes = 0;
-		while (bytes >= 0) {
+		while(bytes >= 0) {
 			memset(buffer, 0, BUF_SIZE);
 			bytes = usb_bulk_read(handle, 0x81, buffer, BUF_SIZE, 1000);
-			if (bytes > 0) {
+			if(bytes > 0) {
 				int i = 0;
 				int next = 0;
-				for (i = 0; i < bytes; i += next) {
+				for(i = 0; i < bytes; i += next) {
 					next = strlen(&buffer[i]) + 1;
-					printf("%s", &buffer[i]);
-					fprintf(fd, "%s", &buffer[i]);
+					fprintf(stdout, "%s", &buffer[i]);
+					if(fd) fprintf(fd, "%s", &buffer[i]);
 				}
 			}
 		}
 
 		char* command = readline("] ");
-		if (command && *command) {
+		if(command && *command) {
 			add_history(command);
 		}
 
-		if (strcmp(command, "/exit")) {
-			if (command[0] != '/') {
-				irecv_sendcmd(handle, command);
-
-			} else {
-				irecv_parsecmd(handle, command);
-			}
-		} else {
-			free(command);
-			break;
-		}
+        if(command[0] == '/') {
+            if(irecv_parse(handle, &command[1]) < 0) {
+                free(command);
+                break;
+            }
+            
+        } else {
+            if(irecv_command(handle, 1, &command) < 0) {
+                free(command);
+                break;
+            }
+        }
 
 		free(command);
 	}
 	
-	fclose(fd);
-	usb_release_interface(handle, 1);
 	free(buffer);
+	if(fd) fclose(fd);
+	usb_release_interface(handle, 1);
 }
 
 void irecv_usage(void) {
 	printf("./irecovery [args]\n");
 	printf("\t-f <file>\t\tupload file.\n");
+	printf("\t-c <command>\t\tsend a single command.\n");
+	printf("\t-k [payload]\t\tsend usb exploit and payload.\n\n");
+	printf("\t-s [logfile]\t\tstarts a shell, and log output.\n");
 	printf("\t-r\t\t\treset usb.\n");
-	printf("\t-c \"command\"\t\tsends a single command.\n");
-	printf("\t-s\t\t\tstarts a shell.\n");
-	printf("\t-k\t\t\tsend usb exploit.\n");
-		printf("\t===============SHELL CMDS===============\n");
-	printf("\t/usbhax\t\t\tsend usb exploit in shell.\n");
-	printf("\t/sendfile\t\tsend file in shell\n");
 }
 
 int main(int argc, char *argv[]) {
-	struct usb_dev_handle *handle = NULL;
 	printf("iRecovery - Recovery Utility\n");
 	printf("by westbaer\nThanks to pod2g, tom3q, planetbeing, geohot and posixninja.\n\n");
-	if (argc < 2) {
+	if(argc < 2) {
 		irecv_usage();
-		return 1;
+		return -1;
 	}
 
-	handle = irecv_init(WTF_MODE);
+	struct usb_dev_handle* handle = irecv_init(RECV_MODE);
 	if (handle == NULL) {
-		handle = irecv_init(RECV_MODE);
-		if (handle != NULL) {
-			printf("Found iPhone/iPod in Recovery mode\n");
+		handle = irecv_init(WTF_MODE);
+		if (handle == NULL) {
+		    printf("No iPhone/iPod found.\n");
+		    return -1;
+		    
+		} else {
+		    printf("Found iPhone/iPod in DFU/WTF mode\n");
 		}
 	} else {
-		printf("Found iPhone/iPod in DFU/WTF mode\n");
+	    printf("Found iPhone/iPod in Recovery mode\n");
 	}
+	
+	if(!strcmp(argv[1], "-f")) {
+	    if(argc == 3) {
+            irecv_upload(handle, argv[2]);
+        }
+            
+	} else if(!strcmp(argv[1], "-c")) {
+	   	if(argc >= 3) {
+	        irecv_command(handle, argc-2, &argv[2]);
+	    }
 
-	if (handle == NULL) {
-		printf("No iPhone/iPod found.\n");
-		return 1;
-	}
+	} else if(!strcmp(argv[1], "-k")) {
+	   	if(argc >= 3) {
+	        irecv_exploit(handle, argv[2]);
+	    } else {
+	        irecv_exploit(handle, NULL);
+	    }
 
-	if (argv[1][0] != '-') {
-		irecv_usage();
-		return 1;
-	}
-
-	if (strcmp(argv[1], "-f") == 0) {
-		if (argc >= 3) {
-			irecv_sendfile(handle, argv[2]);
-
-		} else {
-			printf("No valid file set.\n");
-		}
-
-	} else if (strcmp(argv[1], "-c") == 0) {
-		if (argc >= 3) {
-			irecv_sendcmd(handle, argv[2]);
-
-		} else {
-			printf("No valid command set.\n");
-		}
-
-	} else if (strcmp(argv[1], "-s") == 0) {
-		irecv_console(handle);
-
-	} else if (strcmp(argv[1], "-r") == 0) {
-		irecv_reset(handle);
-
-	} else if (strcmp(argv[1], "-k") == 0) {
-		printf("usbhax 0x21-2-0-0: %04x\n", usb_control_msg(handle, 0x21, 2, 0,
-				0, 0, 0, 1000));
-
-	}
+	} else if(!strcmp(argv[1], "-s")) {
+	   	if(argc >= 3) {
+            irecv_console(handle, argv[2]);
+	    } else {
+	        irecv_console(handle, NULL);
+	    }
+	    
+	} else if(!strcmp(argv[1], "-r")) {
+        irecv_reset(handle);
+	} 
 	
 	irecv_close(handle);
 	return 0;
