79bf2aa247426fdef66fce3c9d99e18573f56456
Jordan <jordan@ubuntu.ubuntu-domain>
2010-04-13 03:00:22 +0100

    I need to test before commiting. Fixed build error.

--- irecovery.c
+++ irecovery.c
@@ -449,7 +449,7 @@ int main(int argc, char *argv[]) {
 	} else if(!strcmp(argv[1], "-r")) {
         irecv_reset(handle);
 	} else if (!strcmp(argv[1], "-l")) {
-	    irec_list(handle, argv[2]);
+	    irecv_list(handle, argv[2]);
 	}
 	
 	irecv_close(handle);
